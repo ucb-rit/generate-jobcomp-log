2746414899484472fd73723c4674980cb7548dde
Viraat Chandra <viraat_chandra_24@berkeley.edu>
2020-06-16 08:27:23 -0700

    batching data

--- jobcomp.py
+++ jobcomp.py
@@ -16,7 +16,10 @@ BASE_URL = 'http://mybrc.brc.berkeley.edu/mybrc-rest/'
 # BASE_URL = 'https://scgup-dev.lbl.gov:8443/mybrc-rest'
 # BASE_URL = 'http://localhost:8880/mybrc-rest'
 
-line_template = '''JobId={jobid} UserId={username}({userid}) GroupId={groupname}(groupid) Name={name} JobState={jobstate} Partition={partition} TimeLimit={timelimit} StartTime={starttime} EndTime={endtime} NodeList={nodelist} NodeCnt={node_count} ProcCnt={proc_count} WorkDir={workdir} ReservationName={reservation_name} Gres={g_res} Account={account} QOS={qos} WcKey={wc_key} Cluster={cluster} SubmitTime={submittime} EligibleTime={eligibletime} ArrayJobId={array_jobid} ArrayTaskId={array_taskid} DerivedExitCode={derived_exitcode} ExitCode={exitcode}'''
+line_template = '''JobId={jobid} UserId={username}({userid}) GroupId={groupname}(groupid) Name={name} JobState={jobstate} Partition={partition} TimeLimit={timelimit} 
+StartTime={starttime} EndTime={endtime} NodeList={nodelist} NodeCnt={node_count} ProcCnt={proc_count} WorkDir={workdir} ReservationName={reservation_name} Gres={g_res} 
+Account={account} QOS={qos} WcKey={wc_key} Cluster={cluster} SubmitTime={submittime} EligibleTime={eligibletime} ArrayJobId={array_jobid} ArrayTaskId={array_taskid} 
+DerivedExitCode={derived_exitcode} ExitCode={exitcode}'''
 # jobid, username, userid, groupname, groupid, name, jobstate, partition,
 # timelimit, starttime, endtime, nodelist, nodecnt, proccnt, workdir,
 # reservationname, gres, account, qos, wckey, cluster, submittime,
@@ -67,15 +70,17 @@ def paginate_req_table(url_function, params=[None, None, None, None]):
         try:
             req = urllib2.Request(url_function(*params, page=page))
             response = json.loads(urllib2.urlopen(req).read())
-            table.extend(response['results'])
+
+            yield response['results']
+            # table.extend(response['results'])
             page += 1
         except urllib2.URLError:
             response['next'] = None
 
-    return table
+    # return table
 
 
-jobs = paginate_req_table(get_job_url)
+# jobs = paginate_req_table(get_job_url)
 
 
 # jobid, username, userid, groupname, groupid, name, jobstate, partition,
@@ -83,46 +88,49 @@ jobs = paginate_req_table(get_job_url)
 # reservationname, gres, account, qos, wckey, cluster, submittime,
 # eligibletime, arrayjobid, arraytaskid, derivedexitcode, exitcode
 
-for job in jobs:
-    jobid = job['jobslurmid']
-    userid = job['userid']
-    jobstate = job['jobstatus']
-    partition = job['partition']
-    starttime = process_date_time(job['startdate'])
-    endtime = process_date_time(job['enddate'])
-    nodelist = job['nodes']
-    nodecnt = job['num_alloc_nodes']
-    proccnt = job['num_cpus']
-    qos = job['qos']
-    submittime = process_date_time(job['submitdate'])
+# for job in jobs:
+for batch in paginate_req_table(get_job_url):
+    for job in batch: 
+        jobid = job['jobslurmid']
+        userid = job['userid']
+        jobstate = job['jobstatus']
+        partition = job['partition']
+        starttime = process_date_time(job['startdate'][:-1])
+        endtime = process_date_time(job['enddate'][:-1])
+        nodelist = job['nodes']
+        nodecnt = job['num_alloc_nodes']
+        proccnt = job['num_cpus']
+        qos = job['qos']
+        submittime = process_date_time(job['submitdate'][:-1])
 
-    # accountid = job['accountid']
+        # accountid = job['accountid']
 
-    # username              : get using userid
-    # groupname
-    # groupid
-    # name
-    # timelimit
-    # wckey
-    # cluster
-    # workdir
-    # reservationname
-    # gres
-    # account               : get using accountid
-    # eligibletime
-    # arrayjobid
-    # arraytaskid
-    # derivedexitcode
-    # exitcode
+        # username              : get using userid
+        # groupname
+        # groupid
+        # name
+        # timelimit
+        # wckey
+        # cluster
+        # workdir
+        # reservationname
+        # gres
+        # account               : get using accountid
+        # eligibletime
+        # arrayjobid
+        # arraytaskid
+        # derivedexitcode
+        # exitcode
 
-    print string.Formatter() \
-        .vformat(line_template, (),
-                 SafeDict(jobid=jobid,
-                          userid=userid,
-                          jobstate=jobstate,
-                          partition=partition,
-                          starttime=starttime, endtime=endtime,
-                          nodelist=str(nodelist),
-                          nodecnt=nodecnt, proccnt=proccnt,
-                          qos=qos,
-                          submittime=submittime))
+        print string.Formatter() \
+            .vformat(line_template, (),
+                     SafeDict(jobid=jobid,
+                              userid=userid,
+                              jobstate=jobstate,
+                              partition=partition,
+                              starttime=starttime, endtime=endtime,
+                              nodelist=str(nodelist),
+                              nodecnt=nodecnt, proccnt=proccnt,
+                              qos=qos,
+                              submittime=submittime))
+
